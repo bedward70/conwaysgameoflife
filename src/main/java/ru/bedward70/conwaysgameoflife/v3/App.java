1c4746c4b7e75470eb118840c83858fcefe3f35d
Eduard Balovnev <bedward70@mail.ru>
2019-12-16 17:48:14 +0600

    Adding of qulice

--- src/main/java/ru/bedward70/conwaysgameoflife/v3/App.java
+++ src/main/java/ru/bedward70/conwaysgameoflife/v3/App.java
@@ -1,5 +1,32 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2019 Eduard Balovnev (bedward70)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package ru.bedward70.conwaysgameoflife.v3;
 
+import java.awt.Label;
+import javax.swing.JToolBar;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 import ru.bedward70.conwaysgameoflife.v3.game.GenGameImpl;
 import ru.bedward70.conwaysgameoflife.v3.paint.PaintStrategySimple;
 import ru.bedward70.conwaysgameoflife.v3.panel.GenPanel;
@@ -8,59 +35,101 @@ import ru.bedward70.conwaysgameoflife.v3.toolbar.RunningButton;
 import ru.bedward70.conwaysgameoflife.v3.toolbar.SpeedSlider;
 import ru.bedward70.conwaysgameoflife.v3.toolbar.TurnButton;
 
-import javax.swing.*;
-import java.awt.*;
-import java.io.IOException;
+/**
+ * The class contains a main entry point.
+ *
+ * @since 0.01
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
+ */
+public final class App {
 
-public class App {
+    /**
+     * Default delay, ms.
+     */
+    private static final int DEFAULT_DELAY_MS = 1000;
 
-    private static int DEFAULT_DELAY = 1000;
+    /**
+     * Min delay, ms.
+     */
+    private static final int MIN_DELAY_MS = 1;
 
-    public static void main(final String... args)
-        throws ClassNotFoundException,
-        UnsupportedLookAndFeelException,
-        InstantiationException,
-        IllegalAccessException,
-        IOException {
+    /**
+     * Max delay, ms.
+     */
+    private static final int MAX_DELAY_MS = 5000;
 
-        final AppAction appAction = new AppAction(DEFAULT_DELAY);
+    /**
+     * Private constructor.
+     */
+    private App() {
+    }
 
+    /**
+     * The main entry point.
+     * @param args Args
+     * @throws Exception Exception
+     */
+    public static void main(final String... args) throws Exception {
+        final AppAction action = new AppAction(App.DEFAULT_DELAY_MS);
         final GenGameImpl game = new GenGameImpl(30, 20);
-        appAction.setTurnGame(() -> game.turn());
-
-        final GenPanel genPanel = new GenPanel(
+        action.setTurnGame(() -> game.turn());
+        final GenPanel panel = new GenPanel(
             32,
             game,
             new PaintStrategySimple()
         );
-        appAction.setRepaintPanel(() -> genPanel.repaint());
-
-        final JToolBar toolBar = new JToolBar();
-        toolBar.setFloatable(false);
-        toolBar.add(new RunningButton(appAction));
-        final Label speedLabel = new Label("" + DEFAULT_DELAY);
-        toolBar.add(new SpeedSlider(1, 5000, DEFAULT_DELAY, value -> {appAction.setUpdateDelay(value);speedLabel.setText("" + value);}));
-        toolBar.add(speedLabel);
-        toolBar.add(new TurnButton(() -> appAction.executeTurn()));
-        toolBar.add(new CleaningButton(() -> appAction.executeClean()));
-
-        final JToolBar statusBar = new JToolBar();
-        final Label cycleLabel = new Label("Cycles: "  + 0);
-        statusBar.add(cycleLabel);
-        appAction.setCycleConsumer(value -> cycleLabel.setText("Cycles: "  + value));
-
+        action.setRepaintPanel(() -> panel.repaint());
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         SwingUtilities.invokeLater(
             () -> {
-
                 new GenFrame(
                     "GenFrame",
-                    toolBar,
-                    genPanel,
-                    statusBar
+                    createToolBar(action),
+                    panel,
+                    createStatusBar(action)
                 );
             }
         );
+    }
 
+    /**
+     * Creates tool bar.
+     * @param action Application action
+     * @return JToolBar instance as the tool bar.
+     */
+    private static JToolBar createToolBar(final AppAction action) {
+        final JToolBar result = new JToolBar();
+        result.setFloatable(false);
+        result.add(new RunningButton(action));
+        final Label speedlabel = new Label(Integer.toString(App.DEFAULT_DELAY_MS));
+        result.add(
+            new SpeedSlider(
+                App.MIN_DELAY_MS,
+                App.MAX_DELAY_MS,
+                App.DEFAULT_DELAY_MS,
+                value -> {
+                    action.setUpdateDelay(value);
+                    speedlabel.setText(Integer.toString(value));
+                }
+            )
+        );
+        result.add(speedlabel);
+        result.add(new TurnButton(() -> action.executeTurn()));
+        result.add(new CleaningButton(() -> action.executeClean()));
+        return result;
+    }
+
+    /**
+     * Creates status bar.
+     * @param action Application action
+     * @return JToolBar instance as the status bar.
+     */
+    private static JToolBar createStatusBar(final AppAction action) {
+        final JToolBar result = new JToolBar();
+        final String template = "Cycles: ";
+        final Label cyclelabel = new Label(String.format(template, 0));
+        result.add(cyclelabel);
+        action.setCycleConsumer(value -> cyclelabel.setText(String.format(template, value)));
+        return result;
     }
 }
--- src/main/java/ru/bedward70/conwaysgameoflife/v3/toolbar/package-info.java
+++ src/main/java/ru/bedward70/conwaysgameoflife/v3/toolbar/package-info.java
@@ -21,27 +21,10 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-package ru.bedward70.conwaysgameoflife.v3;
 
-import ru.bedward70.conwaysgameoflife.v3.panel.GenPanel;
-
-import javax.swing.*;
-import java.awt.*;
-
-public class GenFrame extends JFrame {
-
-    public GenFrame(String title, JToolBar toolBar, GenPanel genPanel, JToolBar statusBar) {
-        super(title);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-
-        add(toolBar, BorderLayout.NORTH);
-        add(genPanel);
-        add(statusBar, BorderLayout.SOUTH);
-
-        pack();
-        setVisible(true);
-    }
-
-
-}
+/**
+ * Nullable.
+ *
+ * @since 0.01
+ */
+package ru.bedward70.conwaysgameoflife.v3.toolbar;
